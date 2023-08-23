6bf358dc46cf62dd763920dacc1de2c5f3a5ea73
willymanoandrews <55712400+willymanoandrews@users.noreply.github.com>
2023-08-23 12:14:59 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, req.url);
+    
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(404);
+            res.end('File not found');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        }
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
